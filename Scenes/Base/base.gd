54e9ffe151416096e18afacb1ea6c8b9be8ebf64
Niamh <niamh.linguist@gmail.com>
2025-03-19 13:23:13 +0200

    base label changes color based on health

--- Scenes/Base/base.gd
+++ Scenes/Base/base.gd
@@ -3,10 +3,14 @@ extends Node3D
 @export var max_health: int = 5 
 @onready var label: Label3D = $Label3D
 
+var RED: Color = Color.RED
+var WHITE: Color = Color.WHITE
+
 var health: int:
 	set(val):
 		health = val
-		label.text = str(health)
+		label.text = str(health) + "/" + str(max_health)
+		label.modulate = RED.lerp(WHITE, float(health) / float(max_health))
 		if health <= 0:
 			get_tree().reload_current_scene()
 
