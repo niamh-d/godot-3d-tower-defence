66767fb73cf4c31209ed5f150df8c46205880392
Niamh <niamh.linguist@gmail.com>
2025-03-23 21:13:54 +0200

    gridmap cells are updated on hover

---
+++ Scenes/RayPickerCamera/ray_picker_camera.gd
@@ -0,0 +1,18 @@
+extends Camera3D
+
+@export var grid_map:GridMap
+
+@onready var ray_cast_3d: RayCast3D = $RayCast3D
+
+func _process(delta: float) -> void:
+	var mouse_pos: Vector2 = get_viewport().get_mouse_position()
+	ray_cast_3d.target_position = project_local_ray_normal(mouse_pos) * 100.0
+	ray_cast_3d.force_raycast_update()
+	
+	if ray_cast_3d.is_colliding():
+		var collider = ray_cast_3d.get_collider()
+		if collider is GridMap:
+			var collision_pt = ray_cast_3d.get_collision_point()
+			var cell = grid_map.local_to_map(collision_pt)
+			if grid_map.get_cell_item(cell) == 0:
+				grid_map.set_cell_item(cell, 1) 
