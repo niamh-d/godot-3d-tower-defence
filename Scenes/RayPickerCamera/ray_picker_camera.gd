9db9beffad752512a64ff4fdf92f9c2eea7f1591
Niamh <niamh.linguist@gmail.com>
2025-03-24 11:50:38 +0200

    cell changes type on click

--- Scenes/RayPickerCamera/ray_picker_camera.gd
+++ Scenes/RayPickerCamera/ray_picker_camera.gd
@@ -12,7 +12,11 @@ func _process(delta: float) -> void:
 	if ray_cast_3d.is_colliding():
 		var collider = ray_cast_3d.get_collider()
 		if collider is GridMap:
-			var collision_pt = ray_cast_3d.get_collision_point()
-			var cell = grid_map.local_to_map(collision_pt)
-			if grid_map.get_cell_item(cell) == 0:
-				grid_map.set_cell_item(cell, 1) 
+			Input.set_default_cursor_shape(Input.CURSOR_POINTING_HAND)
+			if Input.is_action_pressed("click"):
+				var collision_pt = ray_cast_3d.get_collision_point()
+				var cell = grid_map.local_to_map(collision_pt)
+				if grid_map.get_cell_item(cell) == 0:
+					grid_map.set_cell_item(cell, 1) 
+	else:
+		Input.set_default_cursor_shape(Input.CURSOR_ARROW)
