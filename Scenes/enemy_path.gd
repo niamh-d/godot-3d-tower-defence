2a9eeb57ac5be9d5038f697d55ac6e6ef40cde92
Niamh <niamh.linguist@gmail.com>
2025-03-27 12:28:24 +0200

    difficulty manager

--- Scenes/enemy_path.gd
+++ Scenes/enemy_path.gd
@@ -1,6 +1,9 @@
 extends Path3D
 
 @export var enemy_scene: PackedScene
+@export var difficulty_manager: DifficultyManager
+
+@onready var timer: Timer = $Timer
 
 func spawn_enemy() -> void:
 	var new_enemy = enemy_scene.instantiate()
@@ -8,4 +11,5 @@ func spawn_enemy() -> void:
 
 
 func _on_timer_timeout() -> void:
+	timer.wait_time = difficulty_manager.get_spawn_time()
 	spawn_enemy()
